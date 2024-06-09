954ff2b0052fb4127d5c97d78d184a2579be07d2
kassem <omarksm09@gmail.com>
2024-06-09 16:16:53 +0300

    Feat: add network workloads

--- modules/network.ts
+++ modules/network.ts
@@ -7,23 +7,60 @@ import {
   IsOptional,
   IsString,
   Length,
-  ValidateNested,
+
 } from "class-validator";
 import { Expose, Type } from "class-transformer";
-class Peer {
- 
+export class Peer {
+
   @Expose() @IsString() @IsNotEmpty() subnet: string;
   @Expose() @IsString() @IsNotEmpty() wireguard_public_key: string;
-  @Expose() @IsString({each: true}) @ArrayNotEmpty() allowed_ips: string[];
+  @Expose() @IsString({ each: true }) @ArrayNotEmpty() allowed_ips: string[];
   @Expose() @IsString() endpoint: string;
 
-  challenge(): string{
+  challenge(): string {
     let out = this.wireguard_public_key
-    out+= this.endpoint
-    out+= this.subnet
-    for(const ip of this.allowed_ips) out+= ip;
-    
+    out += this.endpoint
+    out += this.subnet
+    for (const ip of this.allowed_ips) out += ip;
+
     return out
   }
 
 }
+
+const MyceliumKeyLen = 32
+class Mycelium {
+  @Expose() @IsString() @IsNotEmpty() @Length(32) key: string;
+  @Expose() @IsOptional() @IsString({ each: true }) peers?: string[];
+
+  challenge(): string {
+    let out = this.key
+    if (!this.peers) return out;
+    for (const peer of this.peers) {
+      out += peer
+    }
+
+    return out
+  }
+}
+
+export class Znet extends Workload {
+  @Export() @IsString() @IsNotEmpty() network_ip_range: string;
+  @Export() @IsString() @IsNotEmpty() subnet: string;
+  @Export() @IsString() @IsNotEmpty() wg_private_key: string;
+  @Export() @IsInt wg_listen_port: number;
+  @Export() @Type(()=>Peer) peers: Peer[];
+  @Export() @Type(()=>Mycelium) mycelium: Mycelium;
+
+
+  challenge(): string {
+      let out = this.network_ip_range;
+      out+= this.subnet;
+      out+= this.wg_private_key;
+      out+= this.wg_listen_port;
+      this.peers.forEach(peer => out += peer.challenge());
+      out+= this.mycelium.challenge();
+      return out;
+  }
+}
+
